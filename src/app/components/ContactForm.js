86882db5827c3c9223bbe5efc14d2794f53d0dc6
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-02-03 01:24:38 +0600

    mongo db connected

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -1,5 +1,7 @@
+'use client'
 import contactStyles from '@/app/contactus/contact.module.css'
 import { Mulish } from 'next/font/google';
+import { useState } from 'react';
 
 const mulish = Mulish({
     weight: ['300', '400', '500', '600', '700', '800', '900'],
@@ -7,44 +9,106 @@ const mulish = Mulish({
     display: 'swap'
 })
 const ContactForm = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        phone: '',
+        message: ''
+    })
+    const [status, setStatus] = useState(null);
+
+    function handleChange (e) {
+        const name = e.target.name 
+        const value = e.target.value
+        
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('../api/contact', {
+                method:'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username:user.username,
+                    email:user.email,
+                    phone:user.phone,
+                    message:user.message
+                })
+            })
+            // Set the status based on the response from the API route
+            if (response.status === 200) {
+                setUser({
+                    username: "",
+                    email: "",
+                    phone: "",
+                    message: ""
+                })
+                setStatus('success');
+            } else {
+                setStatus('error');
+            }
+
+        }catch (e) {
+            console.log(e)
+        }
+
+    }
+
+
+   
     return (
-        <form className={contactStyles.contact_form}>
+        <form className={contactStyles.contact_form} onSubmit={handleSubmit}>
             {/* username */}
             <div className={contactStyles.input_field}>
-               
+
                 <label htmlFor='username' className={contactStyles.label}>
-                Enter Your Name
-                    <input type='text' name='username' id='username' placeholder='Enter Your Name' className={mulish.className}/>
+                    Enter Your Name
+                    <input type='text' name='username' id='username' placeholder='Enter Your Name' className={mulish.className}
+                     value={user.username} onChange={handleChange} autoComplete='off' required/>
                 </label>
             </div>
             {/* email */}
             <div className={contactStyles.input_field}>
-               
-               <label htmlFor='email' className={contactStyles.label}>
-               Enter Your Email
-                   <input type='text' name='email' id='emal' placeholder='example@email.com' className={mulish.className}/>
-               </label>
-           </div>
-            {/* phone number */}
-           <div className={contactStyles.input_field}>
-               
-               <label htmlFor='phone' className={contactStyles.label}>
-               Enter Your Phone Number
-                   <input type='text' name='phone' id='phone' placeholder='Enter Your Contact Number' className={mulish.className}/>
-               </label>
-           </div>
-            {/* messege */}
-           <div className={contactStyles.input_field}>
-               
-               <label htmlFor='messege' className={contactStyles.label}>
-               Enter Your Messege Here
-                   <textarea rows={5} name='messege' id='messege' placeholder='Your Messege Here' className={mulish.className}/>
-               </label>
-           </div>
 
-           <div>
-                <button type='submit' className={mulish.className}>Send Messege</button>
-           </div>
+                <label htmlFor='email' className={contactStyles.label}>
+                    Enter Your Email
+                    <input type='text' name='email' id='emal' placeholder='example@email.com' className={mulish.className} 
+                    value={user.email} onChange={handleChange} autoComplete='off' required/>
+                </label>
+            </div>
+            {/* phone number */}
+            <div className={contactStyles.input_field}>
+
+                <label htmlFor='phone' className={contactStyles.label}>
+                    Enter Your Phone Number
+                    <input type='number' name='phone' id='phone' placeholder='Enter Your Contact Number' className={mulish.className} 
+                    value={user.phone} onChange={handleChange} autoComplete='off' required/>
+                </label>
+            </div>
+            {/* message */}
+            <div className={contactStyles.input_field}>
+
+                <label htmlFor='message' className={contactStyles.label}>
+                    Enter Your Message Here
+                    <textarea rows={5} name='message' id='message' placeholder='Your Message Here' className={mulish.className} 
+                    value={user.message} onChange={handleChange} autoComplete='off' required/>
+                </label>
+            </div>
+
+            <div>
+                 {status === 'success' && <p className={contactStyles.success_msg}>Thank you for your message!</p>}
+                {status === 'error' && <p className={contactStyles.error_msg}>There was an error submitting your message. Please try again.</p>}
+                <button type='submit' className={mulish.className}>Send Message</button>
+            </div>
         </form>
     );
 };
--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -14,6 +14,9 @@ const page = () => {
                     <ContactForm></ContactForm>
                 </section>
             </div>
+
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50825.64371584244!2d-121.99273492093323!3d37.23340465235893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808e34365377033f%3A0xef675301ac748ca6!2sLos%20Gatos%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1706888954630!5m2!1sen!2sbd" width={600} height={450} style={{border:0}} allowfullscreen="" loading="lazy" className={contactStyles.mapping}  referrerpolicy="no-referrer-when-downgrade"></iframe>
         </>
     );
 };
